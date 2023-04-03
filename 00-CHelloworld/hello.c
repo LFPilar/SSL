710ac0a7742000c6ee1a45727e967fd5d59eb9b6
Leandro Pilar <lpilar@sparkgy.com>
2023-04-03 14:08:56 -0300

    Actualizacion archivo readme, creación código hello.c cumpliendo con paso 8 y creacion del hello.exe

---
+++ 00-CHelloworld/hello.c
@@ -0,0 +1,13 @@
+/* "Hello, world!"
+* Al ejecutarse el programa genera un texto con el mensaje "Hello, world!" dentro de un archivo llamado output.txt
+* Leandro Pilar / Usuario Github: LFPilar
+* 02-04-2023
+*/
+
+#include <stdio.h>
+
+int main(){
+    // printf() muestra la cadena de texto entre comillas
+   printf("Hello, World!");
+   return 0;
+}
