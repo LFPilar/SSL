87fdae872cdaae65b3f15c82b3aee3732e2de2b1
Leandro Pilar <lpilar@frba.utn.edu.ar>
2023-04-03 17:06:07 -0300

    se crea el código según finalidad: envío de mensaje a consola

---
+++ 00-CHelloworld/hello_consola.c
@@ -0,0 +1,13 @@
+/* "Hello, world!"
+* Al ejecutarse el programa genera un texto con el mensaje "Hello, world!" y lo muestra en la consola.
+* Leandro Pilar / Usuario Github: LFPilar
+* 02-04-2023
+*/
+
+#include <stdio.h>
+
+int main(){
+    // printf() muestra la cadena de texto entre comillas
+   printf("Hello, World!");
+   return 0;
+}
