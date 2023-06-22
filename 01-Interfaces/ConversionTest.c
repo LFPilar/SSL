485b9348f9b57dda0177f80b1913b9d2e06c3487
LFPilar <lpilar@frba.utn.edu.ar>
2023-06-22 12:28:03 -0300

    Mejoras en archivos ConversionTest.c y TablasDeConversion.c

--- 01-Interfaces/ConversionTest.c
+++ 01-Interfaces/ConversionTest.c
@@ -1,59 +1,49 @@
 /* Conversion.c
 * Test de Prueba de Conversión de Temperatura.
 * Leandro F. Pilar - Legajo 105702/9
-* 20/06/2023
+* 22/06/2023
 */
 
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h> // requerido para manejar función abs
 #include "Conversion.h"
 
 int main(void) {
 
-/* Algunos casos de conversión de temperaturas °F -> °C: */
-printf("\n\n");
-printf("Casos de Conversion de F a C:\n");
-printf("-------------------------------\n\n");
+double casos[8] = {32, 79, 104, 212, 0, 90, 180, 300}; // de 0 a 3: conversión a Celsius, de 4 a 7: conversión a Farenheit
+double resultadosEsperados[8] = {0, 26.111, 40.000, 100.000, 32.000, 194.000, 356.000, 572.000};
 
-printf ("32 F representan 0.000 C:\n"); 
-assert(Celsius(32) == 0); 
-printf ("Calculo verificado.\n\n");
+for (int a = 0; a<8; a++){
+    if (a==0) {
+        printf("\n");
+        printf("Casos de Conversion de grados F a grados C:\n");
+        printf("-------------------------------------------\n\n");        
+    }
 
-// una manera de evaluar conversiones por aproximación de un valor con decimales...
-printf ("79 F representan 26.111 C:\n"); 
-assert(Celsius(79) <= 26.112); 
-assert(Celsius(79) >= 26.110); 
-printf ("Calculo verificado.\n\n");
+    if (a==4) {
+        printf("\n");
+        printf("Casos de Conversion de grados C a grados F:\n");
+        printf("-------------------------------------------\n\n");        
+    }
 
-printf ("104 F representan 40.000 C:\n"); 
-assert(Celsius(104) == 40); 
-printf ("Calculo verificado.\n\n");
+    if (a<4) {
+        if (resultadosEsperados[a] != abs(resultadosEsperados[a])) {
+            // una manera de evaluar conversiones por aproximación de un valor con decimales...
+            assert(Celsius(casos[a]) <= resultadosEsperados[a]+0.001); 
+            assert(Celsius(casos[a]) >= resultadosEsperados[a]-0.001);
+        } else assert(Celsius(casos[a]) == resultadosEsperados[a]); 
+    } else {
+        if (resultadosEsperados[a] != abs(resultadosEsperados[a])) {
+            assert(Farenheit(casos[a]) <= resultadosEsperados[a]+0.001); 
+            assert(Farenheit(casos[a]) >= resultadosEsperados[a]-0.001);
 
-printf ("212 F representan 100.000 C:\n"); 
-assert(Celsius(212) == 100); 
-printf ("Calculo verificado.\n");
+        } else assert(Farenheit(casos[a]) == resultadosEsperados[a]); 
+    }
 
+    printf ("%.3f %20s %.3f \n", casos[a], " grados representan ", resultadosEsperados[a]); 
+    printf ("Calculo verificado.\n\n");
+}
 
-/* Algunos casos de conversión de temperaturas °C -> °F: */
-printf("\n\n");
-printf("Casos de Conversion de C a F:\n");
-printf("-------------------------------\n\n");
-
-printf ("0 C representan 32.000 F:\n"); 
-assert(Farenheit(0) == 32.0);
-printf ("Calculo verificado.\n\n");
-
-printf ("90.0 C representan 194.000 F:\n"); 
-assert(Farenheit(90.0) == 194);
-printf ("Calculo verificado.\n\n");
-
-printf ("180 C representan 356.000 F:\n"); 
-assert(Farenheit(180) == 356);
-printf ("Calculo verificado.\n\n");
-
-printf ("300 C representan 572.000 F:\n"); 
-assert(Farenheit(300) == 572);
-printf ("Calculo verificado.\n\n");
-
- return 0;
+return 0;
 }
--- 01-Interfaces/TablasDeConversion.c
+++ 01-Interfaces/TablasDeConversion.c
@@ -4,35 +4,99 @@
 *   Conversion.h -> le indica a este archivo qué pares de temperatura debe imprimir para cada tabla de conversión. 
     Esto significa que TablasDeConversion.c se abstrae del modo en que se calculan los valores.
 *   stdio.h -> le indica al compilador que debe incluir info referida al modo en que debe mostrarse en pantalla la info.
+
+* Comentarios adicionales: se trabaja sobre los objetivos fijados en Créditos Extra: 
+
+   Desarrollar la función PrintFilas para que sea genérica, es decir, pueda invocarse desde PrintTablaFahrenheit y desde PrintTablaCelsius. PrintFilas debe invocar a PrintFila. Considere el uso de punteros a función.""
+
+Sobre esto, vale decir que esta consigna se considera lo suficientemente abierta como para que sean admitidas varias posibles implementaciones. En este sentido, el desdoblamiento de una función de impresión (tal como se había considerado en el primer commit) en varias funciones que trabajan en cascada, solo responde a lo que se interpretó de dicho requerimiento. No obstante, no es una opción que personalmente habría preferido, porque complejiza la estructura del programa sin ofrecer mayores beneficios, al menos en la órbita de lo que se interpretó en el pedido.
+
 * Leandro Pilar - Legajo 105702/9
-* 20/06/2023
+* 22/06/2023
  */
 
 #include <stdio.h>
 #include "Conversion.h"
+#include <stdbool.h>
 
 /* Recordatorio: la diferencia entre las dos formas de la instrucción #include "Conversion.h" y #include <stdio.h> es que la primera busca el archivo ‘Conversion.h’ en el directorio actual antes de buscarlo en los directorios de los archivos de cabeceras del sistema. La instrucción de inclusión #include <... .h> busca entre los archivos de cabeceras del sistema, pero no busca en el directorio actual.*/
 
-int main(void){
+//-----------------------------------------------------------------------------------------------
+//Funciones de Impresión de Tablas
 
- const int MINIMO = 0; // cota inferior de temperaturas
- const int MAXIMO = 300; // cota superior
- const int DELTA = 10; // salto de termperaturas en el rango estudiado
+void PrintEncabezado()
+{
+   printf("\n\n");
+   printf("Tabla de Temperaturas: Celsius -> Farenheit                    Tabla de Temperaturas: Farenheit -> Celsius\n");
+   printf("Grados Centigrados    |   Grados Farenheit                      Grados Farenheit    |    Grados Centigrados\n");
+   printf("------------------------------------------                      ------------------------------------------- \n");   
+}
 
-printf("\n\n");
-printf("Tabla de Temperaturas: Celsius -> Farenheit                    Tabla de Temperaturas: Farenheit -> Celsius\n");
-printf("Grados Centigrados    |   Grados Farenheit                      Grados Farenheit    |    Grados Centigrados\n");
-printf("------------------------------------------                      ------------------------------------------- \n");
+void PrintFila (int nroTabla){
+   if (nroTabla ==1)
+   printf("%24s", ""); // separador de tablas
+   else
+   printf("------------------------------------------                      ------------------------------------------- \n");
+}
 
- for(int temperatura = MINIMO; temperatura <= MAXIMO; temperatura = temperatura + DELTA)
- {
-    /* invoca función de conversión de °C a °F*/
-    printf("%20d %1s %18.3f", temperatura, " |", Farenheit(temperatura));
+void PrintFilas (int nroTabla, double *temperatura) {
+if (nroTabla ==1) {
+   printf("%20.3f %1s %18.3f", *temperatura, " |", Farenheit(*temperatura)); // invoca función de conversión de °C a °F
+} else {
+   printf("%16.3f %1s %21.3f\n", *temperatura, " |", Celsius(*temperatura)); // invoca función de conversión de °F a °C  
+}
+   PrintFila(nroTabla);
+}
 
-    printf("%24s", ""); 
-
-    /* invoca función de conversión de °F a °C*/
-    printf("%16d %1s %21.3f\n", temperatura, " |", Celsius(temperatura));
+void PrintTablaCelsius (double *temperatura) {
+   PrintFilas(1, temperatura);
  }
 
+void PrintTablaFarenheit (double *temperatura) {
+   PrintFilas(2, temperatura);
 }
+
+
+void PrintTablas (double *pminimo, double *pmaximo, double *pdelta) {
+PrintEncabezado();
+   for(double temperatura = *pminimo; temperatura <= *pmaximo; temperatura = temperatura + *pdelta)
+ { 
+   PrintTablaCelsius(&temperatura);
+   PrintTablaFarenheit(&temperatura);
+ }
+}
+
+//---------------------------------------------------------------------------------
+
+int main(void){
+
+double minimo, maximo, delta, ip;
+
+printf("Limite Inferior: ");
+scanf("%lf", &ip); // cota inferior de temperaturas
+minimo = ip;
+
+do {
+   printf("\nLimite Superior: ");
+   scanf("%lf", &ip); // cota superior
+   maximo = ip;
+   if (maximo < minimo) printf("Cota superior debe ser mayor a la cota inferior...\n");
+   
+} while (maximo < minimo);
+
+do {
+   printf("\nDelta: ");
+   scanf("%lf", &ip); // salto de temperaturas en el rango estudiado
+   delta = ip;
+   if (delta > (maximo - minimo)) printf("Valor del delta debe ser inferior al rango de cotas...\n");
+} while (delta > (maximo - minimo));
+
+PrintTablas (&minimo, &maximo, &delta);
+
+printf("\nComandos p/ correr el Test de Calculo: 1) make Test 2) Test.\n");
+printf("Comando p/ borrar archivos intermedios y ejecutables: make borrar.\n");
+
+}
+
+
+
